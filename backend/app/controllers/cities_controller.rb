a1efbc1b38334cdac2591dae44bb6e35920b897d
daliayan <daliayan5@gmail.com>
2021-08-04 21:53:24 -0400

    Added skeleton to cities controller - created serializer file

--- backend/app/controllers/cities_controller.rb
+++ backend/app/controllers/cities_controller.rb
@@ -1,2 +1,8 @@
 class CitiesController < ApplicationController
+
+    def index 
+        cities = City.all
+        #need to render json here
+    end
+
 end
--- backend/app/controllers/restaurants_controller.rb
+++ backend/app/controllers/restaurants_controller.rb
@@ -1,2 +1,36 @@
-class Restaurants < ApplicationController
+class RestaurantsController < ApplicationController
+
+    def index
+        restaurants = Restaurant.all
+        #need to render json here
+    end
+
+    def show
+        restaurant = Restaurant.find(params[:id])
+        # need to render json here
+    end
+
+    def create
+        restaurant = Restaurant.new(restaurant_params)
+
+        if restaurant.save
+            #render json
+        else
+            #render json
+        end
+        
+    end
+
+    def destroy
+        restaurant = Restaurant.find(params[:id])
+        restaurant.destroy
+        # need to render json here
+    end
+
+    private
+
+    def restaurant_params
+        params.require(:restaurant).permit(:name, :review, :star, :price)
+    end
+
 end
