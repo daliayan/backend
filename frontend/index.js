47ccb568e3829e07f4d4cea5d6ec2dc4345a3baf
daliayan <daliayan5@gmail.com>
2021-08-11 20:41:51 -0400

    was able to fetch restaurant data

--- frontend/index.js
+++ frontend/index.js
@@ -5,3 +5,6 @@ const cityApi = new CityApi('http://localhost:3000/cities')
 document.addEventListener('DOMContentLoaded', () => {
     cityApi.getCities();
 });
+
+const restaurantApi = new RestaurantApi('http://localhost:3000/restaurants')
+restaurantApi.getRestaurants();
